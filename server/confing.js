4861381315475bac1ccb6a4b82ec7c4bff3c5127
Rosa Llorente <rosamariallorentemojica.com>
2025-12-17 08:19:43 +0100

    retoque variables de entorno3

--- server/confing.js
+++ server/confing.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-export const PORT = process.env.PORT || 8080;
+export const PORT = process.env.PORT || 3000;
 
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
